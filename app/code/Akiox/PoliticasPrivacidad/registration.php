eb81dbe427ac375f25660a14a08e73fd6cbf9da7
farobledo <fabian.robledolave@gmail.com>
2022-04-26 21:14:47 -0500

    module_Politicas_Privacidad

---
+++ app/code/Akiox/PoliticasPrivacidad/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Akiox_PoliticasPrivacidad',
+        __DIR__
+    );
