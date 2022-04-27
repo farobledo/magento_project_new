59d0f894df46325c9b521e9e733a4b8721d2133a
farobledo <fabian.robledolave@gmail.com>
2022-04-27 09:06:19 -0500

    module

--- app/code/Akiox/PoliticasPrivacidad/Controller/Index/Index.php
+++ app/code/Akiox/PoliticasPrivacidad/Controller/Index/Index.php
@@ -4,6 +4,12 @@ namespace Axiox\PoliticasPrivacidad\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /** @var \Magento\Framework\View\Result\PageFactory */     protected $resultPageFactory;
+    public function __construct(\Magento\Framework\App\Action\Context $context,         \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
     /**
      * Index action
      *
@@ -11,6 +17,7 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
     }
 }
