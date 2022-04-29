0d3eb44d046a874bed06ba7380f758773a6cfad4
farobledo <fabian.robledolave@gmail.com>
2022-04-28 19:29:25 -0500

    cambios nuevos del modulo

---
+++ app/code/Akios/PoliticasPrivacidad/Controller/Index/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Akios\PoliticasPrivacidad\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * Index action
+     *
+     * @return $this
+     */
+    public function execute()
+    {
+
+    }
+}
