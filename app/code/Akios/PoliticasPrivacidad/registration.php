d298fd5832c82d1a51a495dccd4a41a66dc0a5f8
farobledo <fabian.robledolave@gmail.com>
2022-05-02 08:56:37 -0500

    modulo instalado

--- app/code/Akios/PoliticasPrivacidad/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Akios\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Akios_PoliticasPrivacidad',
-        __DIR__
-    );
